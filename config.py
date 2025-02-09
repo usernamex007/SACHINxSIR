9ae122ec8f4257275c0926cf4034075ab5cf706c
usernamex007 <sonalixsachin@gmail.com>
2025-02-09 22:54:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+API_ID = '28795512'  # Your API ID
+API_HASH = 'c17e4eb6d994c9892b8a8b6bfea4042a'  # Your API Hash
+BOT_TOKEN = '7610510597:AAFX2uCDdl48UTOHnIweeCMms25xOKF9PoA'  # Your Bot Token
